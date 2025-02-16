c912749debb9af76a74ff17df0d368ee552b42bd
Anupam <9582anupamk@gmail.com>
2025-02-16 18:18:06 +0530

    more features

--- Backend/routes/v1/index.js
+++ Backend/routes/v1/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import usersRoutes from "./users.routes.js";
 import schemesRoutes from "./schemes.routes.js";
+import recommendRoutes from "./recommendations.routes.js";
+import chatbotRoutes from "./chatbot.routes.js";
 
 
 const router = express.Router();
@@ -8,6 +10,8 @@ const router = express.Router();
 // Attach specific routes for version 1
 router.use("/users", usersRoutes);
 router.use("/schemes", schemesRoutes);
+router.use("/recommendations", recommendRoutes);
+router.use("/chatbot", chatbotRoutes);
 router.use("/", (req, res) => {
     res.send("API V1 Running");
 })
