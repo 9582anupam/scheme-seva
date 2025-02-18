63c39fae9dd0703601a579596d12e095402d8e5e
Anupam <9582anupamk@gmail.com>
2025-02-19 04:56:57 +0530

    fix deploy

--- Frontend/src/components/pages/schemeDetailss/SchemeDetailss.js
+++ Frontend/src/components/pages/schemeDetailss/SchemeDetailss.js
@@ -13,7 +13,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { toast } from "react-hot-toast";
 
-const SchemeDetails = () => {
+const SchemeDetailss = () => {
     const { id } = useParams();
     // const navigate = useNavigate();
     const [scheme, setScheme] = useState(null);
@@ -344,4 +344,4 @@ const SchemeDetails = () => {
     );
 };
 
-export default SchemeDetails;
+export default SchemeDetailss;
